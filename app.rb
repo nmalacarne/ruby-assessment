2123897495fda258a4885a8c9ec8b6c3913d0680
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2015-09-04 16:14:50 -0400

    created models; added model relationships

--- app.rb
+++ app.rb
@@ -5,3 +5,20 @@ ActiveRecord::Base.logger = Logger.new(STDERR)
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
 
+class User < ActiveRecord::Base
+  has_one :address
+  has_one :company
+end
+
+class Address < ActiveRecord::Base
+  belongs_to :user
+  has_one :geo
+end
+
+class Geo < ActiveRecord::Base
+  belongs_to :address
+end
+
+class Company < ActiveRecord::Base
+  belongs_to :user
+end
