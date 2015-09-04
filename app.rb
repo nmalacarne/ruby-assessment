15ab940c48895560f8ed08a5f1159d974780dd70
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2015-09-04 16:12:54 -0400

    using sqlite3 in memory

--- app.rb
+++ app.rb
@@ -1,3 +1,7 @@
 require 'active_record'
 require 'sqlite3'
 
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
+
