03400c6357a1975ed316627b7fa97272ec768a0d
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2015-09-04 16:22:20 -0400

    added migrations for users, addresses, geos, and copmanies tables

--- app.rb
+++ app.rb
@@ -5,6 +5,37 @@ ActiveRecord::Base.logger = Logger.new(STDERR)
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
 
+ActiveRecord::Schema.define do
+  create_table :users do |t|
+    t.column :name, :string
+    t.column :username, :string
+    t.column :email, :string
+    t.column :phone, :string
+    t.column :website, :string
+  end
+
+  create_table :addresses do |t|
+    t.column :user_id, :integer
+    t.column :street, :string
+    t.column :suite, :string
+    t.column :city, :string
+    t.column :zipcode, :string
+  end
+
+  create_table :geos do |t|
+    t.column :address_it, :integer
+    t.column :lat, :string
+    t.column :lng, :string
+  end
+
+  create_table :companies do |t|
+    t.column :user_id, :integer
+    t.column :name, :string
+    t.column :catchPhrase, :string
+    t.column :bs, :string
+  end
+end
+
 class User < ActiveRecord::Base
   has_one :address
   has_one :company
